917fcd61404f39c2158f2603c693c554fcb870fa
Lillys Hernandez <lillys.hernandez.ramos@gmail.com>
2022-11-09 21:51:28 -0600

    Proyecto Final Carrito

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+//Todos los elementos del DOM que voy a necesitar para crear las tarjetas de productos
+const contTarjetas = document.getElementById('tarjetas');
+
+//---------FETCH-----------
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('productos.json')
+        const data = await res.json()
+        //console.log(data)
+        mostrarInfoMascota(data);
+        addToCarritoItem(data);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+//Creo HTML dinámico para mostrar la información de los productos para mascotas a partir del array fake DB
+function mostrarInfoMascota(data) {
+    contTarjetas.innerHTML = ''
+    data.forEach(element => {
+        let html = `<div class="col d-flex justify-content-center mb-4">
+        <div class="card shadow mb-1 rounded" style="width: 20rem;">
+            <h5 class="card-title pt-2 text-center text-primary">${element.titulo}</h5>
+            <img src="${element.img}" class="card-img-top" alt="${element.alt}">
+            <div class="card-body">
+                <p class="card-text text-white-50 description">${element.descripcion}</p>
+                <h5 class="text-primary">Precio: <span class="precio">${element.precio}</span></h5>
+                <div class="d-grid gap-2">
+                    <button class="btn btn-primary button" onclick="addToCarritoItem(${element.id})">Añadir a Carrito</button>
+                </div>
+            </div>
+        </div>
+        </div>`;
+        contTarjetas.innerHTML += html;
+    });
+
+}
+
+
+//-------CARRITO DE COMPRAS-------------
 const Clickbutton = document.querySelectorAll('.button')
 const tbody = document.querySelector('.tbody')
 let carrito = []
@@ -7,9 +52,96 @@ Clickbutton.forEach(btn => {
 })
 
 
-function addToCarritoItem(e) {
+function addToCarritoItem(data) {//2
+    let producto = data.find(item => item.id === id);
+    console.log(producto);
+    //addItemCarrito(producto)
+}
+
+/*function addToCarritoItem(data) {
+    const Clickbutton = document.querySelectorAll('.button')
+    //console.log(Clickbutton);
+    Clickbutton.forEach(btn => {
+        btn.addEventListener('click', addToCarritoItem => {
+            console.log(btn.id)
+        })
+
+    })
+}*/
+
+
+
+/*function addItemCarrito(newItem) {
+    carrito.push(newItem);
+    console.log(carrito);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    //renderCarrito()
+
+}*/
+
+
+/*function renderCarrito() {
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
+    console.log(carrito);
+    const tabla_carrito = document.getElementById("tabla_carrito");
+    tabla_carrito.innerHTML = ''
+    carrito.map(item => {
+        const tr = document.createElement('tr')
+
+        tr.classList.add('ItemCarrito')
+
+        const content = `
+        <th scope="row">${item.id}</th>
+        <td class="tabla__productos">
+            <img src=${item.img} alt="">
+            <h6 class="title">${item.titulo}</h6>
+        </td>
+        <td class="tabla__price">
+            <p>${item.precio}</p>
+        </td>
+        <td class="tabla__cantidad">
+            <input class="input__elemento" type="number" min="1" value=${item.cantidad}>
+            <button class="delete btn btn-danger">x</button>
+        </td>
+    `
+        tr.innerHTML = content;
+        tabla_carrito.append(tr)
+    })
+
+    document.getElementById("total_carrito").innerHTML = "$" + precioTotal();
+    //precioTotal()
+}
+
+function precioTotal() {
+    let carritos = JSON.parse(localStorage.getItem("carrito"));
+
+
+    let carr = (carritos.reduce((total, item) => (total += item.precio, 0)));
+    return carr
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function addToCarritoItem(e) {
     const button = e.target
-    const item = button.closest('.card')
+    const item = button('.card')
     const itemTitle = item.querySelector('.card-title').textContent;
     const itemPrice = item.querySelector('.precio').textContent;
     const itemImg = item.querySelector('.card-img-top').src;
@@ -136,4 +268,4 @@ window.onload = function () {
         carrito = storage;
         renderCarrito()
     }
-}
+}*/
